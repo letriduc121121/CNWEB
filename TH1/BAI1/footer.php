0c32e36745e74cf9131c0ab28a15a35f9179e083
LeTriDuc121121 <letriduc121121@gmail.com>
2024-11-29 23:59:22 +0700

    xoa

--- TH1/BAI1/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="text-center py-3 mt-4 border-top">
-<p style="font-weight: bold; font-size: 30px;"> © 2024 Các loài hoa phù hợp xu hướng </p>
-</footer>
-    <script src="./bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
