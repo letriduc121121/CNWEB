b4d025c19ae75a55d08bb5091452aa36b3903b64
LeTriDuc121121 <letriduc121121@gmail.com>
2024-11-30 00:04:03 +0700

    load

---
+++ BTTH/TH1/BAI1/footer.php
@@ -0,0 +1,6 @@
+<footer class="text-center py-3 mt-4 border-top">
+<p style="font-weight: bold; font-size: 30px;"> © 2024 Các loại hoa phù hợp với bạn.</p>
+</footer>
+    <script src="./bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
